6b0d46b307248fe4e15c59f9d7fd96d72a3b7839
kylieeXD <khayloaftck@gmail.com>
2026-02-01 19:03:32 +0000

    Revert surya: Include firmware files
    This reverts commit cd703c23de2a2343b2dff0a1fd0e3aabf4fc3e64.

--- device.mk
+++ device.mk
@@ -62,9 +62,6 @@ PRODUCT_COPY_FILES += \
 # FM
 BOARD_HAVE_QCOM_FM := true
 
-# Firmware
-$(call inherit-product, vendor/xiaomi/firmware/config.mk)
-
 # GSI keys
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
