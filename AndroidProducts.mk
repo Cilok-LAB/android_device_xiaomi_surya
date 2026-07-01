f43d63619bfcaedcead6aef424c737cba01a8b7b
kylieeXD <khayloaftck@gmail.com>
2026-07-01 03:01:08 +0000

    Revert "surya: LumineDroid Bringup"
    This reverts commit b9bc258e2727cb7e6dae9f1f64783635651cedc8.

---
+++ AndroidProducts.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2020-2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_surya.mk \
+    $(LOCAL_DIR)/custom_surya.mk
+
